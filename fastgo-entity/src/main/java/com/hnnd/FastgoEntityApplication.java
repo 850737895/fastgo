acba093bca8df61686a92b2bbabc0868583bc5be
zhuwei <850737895@qq.com>
2018-11-13 10:22:54 +0800

    '初始化fastgo-entity工程目录'

---
+++ fastgo-entity/src/main/java/com/hnnd/FastgoEntityApplication.java
@@ -0,0 +1,12 @@
+package com.hnnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastgoEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastgoEntityApplication.class, args);
+	}
+}
